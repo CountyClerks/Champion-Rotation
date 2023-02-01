05abb107aa8ba7c545f747278ee33c502f11b637
Eric Hardy <erichardy011@gmail.com>
2023-01-31 23:33:42 -0600

    Added all card assets for champion thumbnails, created components for Game, Rules and Header

---
+++ src/components/Game.jsx
@@ -0,0 +1,19 @@
+
+import {useEffect, useState} from 'react'
+import champions from '../utils/championArray'
+export default function Game() {
+        const [championArray, setChampions] = useState({});
+
+        const [score, setScore] = useState(0);
+
+        // const [allChampions, setAllChampions] = useState([])
+
+        function getChampionImage() {
+            
+        }
+
+        return (
+            <div>
+            </div>
+        )
+}
