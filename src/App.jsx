78b43c839f90e8172fef3558ae0e65f98af9e1c2
Eric Hardy <erichardy011@gmail.com>
2023-02-01 00:22:20 -0600

    Added fonts folder and background image for body

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import Header from './components/Header'
+import Game from './components/Game'
 
 function App() {
 
   return (
     <div className="App">
+      <Header />
+      <Game />
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <h1 className="header--title">The Champion Rotation</h1>
+            {/* <h2 className="header--subtitle">Select a champion you haven't summoned yet.</h2> */}
+        </header>
+    )
+}
